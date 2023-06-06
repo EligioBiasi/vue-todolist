676386febdf83bce346116045e4d4d1bc07adf32
Eligio Biasi <ex_98@live.it>
2023-06-06 15:52:52 +0200

    removed todo item function added

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ createApp({
         },
         {   
             text:'calcietto',
-            done:false,
+            done:true,
         },
         {   
             text:'leggere',
@@ -22,20 +22,29 @@ createApp({
         },
         {   
             text:'vedere film',
-            done:false,
+            done:true,
         },
         {   
             text:'andare dal barbiere',
             done:false,
+        },
+        {   
+            text:'stirare',
+            done:true,
+        },
+        {   
+            text:'lavare i piatti',
+            done:true,
         },
       ]
     }
   },
 
   methods: {
-
+    removeListItem(itemIndex){
+        this.todolist.splice(itemIndex, 1)
+    }
   },
-
 
 }).mount('#app');
 
@@ -43,9 +52,7 @@ createApp({
 
 
 
-// MILESTONE 1
-// Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
+
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
